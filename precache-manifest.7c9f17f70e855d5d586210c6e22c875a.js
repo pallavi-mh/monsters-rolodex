e98881a18e326009e84ef52c3a6e199235576f96
pallavimh <pallavi@graniteriverlabs.in>
2020-09-03 18:44:01 +0530

    Updates

---
+++ precache-manifest.7c9f17f70e855d5d586210c6e22c875a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29076d7ca2908027187275f2abe4b2bd",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ce0bc7000ab2e2a0715f",
+    "url": "/monsters-rolodex/static/css/main.89c3401c.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce0bc7000ab2e2a0715f",
+    "url": "/monsters-rolodex/static/js/main.6f896edc.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
